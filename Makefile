59cab84b3d070ed7164ef3513c3d760745bf4fa8
ZzDaBing <guillaume.bigand@ens.uvsq.fr>
2021-12-27 18:26:02 +0100

    Update from branch master + Working on python test function for mutation. git status! NOT FINISHED YET git status!

--- Makefile
+++ Makefile
@@ -4,24 +4,37 @@ CFLAGS = -g -std=c99
 
 LDFLAGS = -lcmocka
 
-.PHONY: clean all
+.PHONY: clean all check
 
 %.o: %.c 
 	$(CC) $(CFLAGS) -c -o $@ $<
 
-all: main
+#For all compiling, building and executing
+all: main DNA check
+
+#For only building and testing interface
+build: DNA test_DNA
 
 main: main.o gene.o
 	$(CC) $(CFLAGS) -o $@ $^
 
 gene.o: gene.h
 
-check: test_gene
+check: test_gene test_DNA
 
 test_gene: test_gene.o
 	$(CC) $(CFLAGS) -o $@ $^ $(LDFLAGS)
 
 test_gene.o: gene.c 
 
-clean:
-	rm -f *.o main test_gene
+DNA : 
+	python3 setup.py build
+	cp build/lib*/*.so .
+
+test_DNA : 
+	python3 -m pytest -s
+
+clean :
+	@rm -rf __pycache__ build .pytest_cache *.so
+	@rm -f *.o main test_gene
+
--- gene.h
+++ gene.h
@@ -1,6 +1,39 @@
-#ifndef GENE_H
-#define GENE_H
+#pragma once 
+#define MAX_GENES  1024
 
-bool detecting_mutations(const unsigned int gene_seq[], const unsigned long long size_sequence);
+typedef struct gene_map_s {
 
-#endif
+    //
+    unsigned long long genes_counter;
+
+    //Gene start position (AUG)
+    unsigned long long* gene_start;
+
+    //Gene stop position (UAA, UAG, UGA)
+    unsigned long long* gene_end;
+
+}gene_map_t;
+
+typedef struct codon {
+    char* codon;
+    char* short_name;
+    char* full_name;
+    char symbol;
+}codon;
+
+typedef struct mutation_map {
+    unsigned long* size;
+    unsigned long *start_mut;
+    unsigned long *end_mut;
+}mutation_map;
+
+//extern struct gene_map_s gene_map;
+unsigned short* convert_to_binary(char* dna_seq, unsigned size);
+char* binary_to_codon(unsigned short* bin_dna_seq, unsigned size);
+char* generating_mRNA(const unsigned short gene_seq [], const unsigned int seq_size);
+void detecting_genes(const unsigned int gene [], const unsigned int gene_size, gene_map_t* gene_map);
+char* generating_amino_acid_chain(const unsigned short gene_seq [], const unsigned int seq_size);
+void detecting_mutations(const unsigned short gene_seq [], const unsigned long size_sequence, mutation_map mut_m);
+float calculating_matching_score(const unsigned short seq1 [], const int sequence_size1, const unsigned short seq2 [], const int sequence_size2);
+int binary_size_count(int b);
+int hamming(int seq1, int seq2);
